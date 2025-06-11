8f7ba5b7c3ff0529b4dcba6d2570e193084939ee
Md Mahfuz Rahman <tanzimrani8@gmail.com>
2025-06-11 21:33:47 +0530

    Add files via upload

---
+++ src/me/arcxdev/scoreboard/Main.java
@@ -0,0 +1,29 @@
+package me.arcxdev.scoreboard;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    private static Main instance;
+    private ScoreboardManager scoreboardManager;
+
+    @Override
+    public void onEnable() {
+        instance = this;
+        saveDefaultConfig();
+
+        if (Bukkit.getPluginManager().getPlugin("PlaceholderAPI") == null) {
+            getLogger().severe("PlaceholderAPI is not installed! Disabling plugin.");
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+
+        scoreboardManager = new ScoreboardManager(this);
+        scoreboardManager.start();
+    }
+
+    public static Main getInstance() {
+        return instance;
+    }
+}
